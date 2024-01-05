6003d4981a58d4ce0bf87f01f107a268e9f92b9e
Jules EVENO laptop <Vous@exemple.com>
2024-01-05 17:25:37 +0100

    maj

--- game/script.sh
+++ game/script.sh
@@ -1,28 +1,2 @@
 #bin/bash
-for ((a=0; a<10; a++))
-    do mkdir $a
-    for ((b=0; b<10; b++))
-        do mkdir $a/$b
-        for ((c=0; c<10; c++))
-            do mkdir $a/$b/$c
-            for ((d=0; d<10; d++))
-                do mkdir $a/$b/$c/$d
-                for ((e=0; e<10; e++))
-                    do mkdir $a/$b/$c/$d/$e
-                    for ((f=0; f<10; f++))
-                        do mkdir $a/$b/$c/$d/$e/$f
-                        for ((g=0; g<10; g++))
-                            do mkdir $a/$b/$c/$d/$e/$f/$g
-                            for ((h=0; h<10; h++))
-                                do echo $a$b$c$d$e$f$g$h >> $a/$b/$c/$d/$e/$f/$g/$h
-                                echo $a$b$c$d$e$f$g$h
-                                sleep 0.1
-                                done
-                            done
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
+for ((a=0; a<10; a++)); do mkdir $a; for ((b=0; b<10; b++)); do mkdir $a/$b; for ((c=0; c<10; c++));do mkdir $a/$b/$c;for ((d=0; d<10; d++));do mkdir $a/$b/$c/$d;for ((e=0; e<10; e++));do mkdir $a/$b/$c/$d/$e;for ((f=0; f<10; f++));do mkdir $a/$b/$c/$d/$e/$f;for ((g=0; g<10; g++));do mkdir $a/$b/$c/$d/$e/$f/$g;for ((h=0; h<10; h++));do echo $a$b$c$d$e$f$g$h >> $a/$b/$c/$d/$e/$f/$g/$h;echo $a$b$c$d$e$f$g$h;sleep 0.1;done;done;done;done;done;done;done;done;done
